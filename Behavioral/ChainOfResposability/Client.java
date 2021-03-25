afc56b2e40c74e5d1ee7d5f96b5f08ffb59893df
Felipe <felipecabezas98@gmail.com>
2021-03-25 11:08:12 -0500

    chain of resposability with scanner

--- Behavioral/ChainOfResposability/Client.java
+++ Behavioral/ChainOfResposability/Client.java
@@ -1,8 +1,16 @@
 package ChainOfResposability;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
-        int cantidad = 120000;
+        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad a retirar");
+        int cantidad = sc.nextInt();
+        sc.close();
+
         ManejadorCambio mc50 = new Billete50();
         ManejadorCambio mc20 = new Billete20();
         ManejadorCambio mc10 = new Billete10();
