d8c79eb81b7b30a0b1517b0160c31b2c15f182cf
Felipe <felipecabezas98@gmail.com>
2021-03-25 15:45:59 -0500

    iterator implemented!

---
+++ Behavioral/Iterator/IIterator.java
@@ -0,0 +1,7 @@
+public interface IIterator
+{
+void First();//Reset to first element
+String Next();//get next element
+Boolean IsDone();//End of collection check
+String CurrentItem();//Retrieve Current Item
+}
---
+++ Behavioral/Iterator/ISubject.java
@@ -0,0 +1,3 @@
+public interface ISubject {
+    public IIterator CreateIterator();
+}
