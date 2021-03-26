8bc28b2005f972ee00e643d02b7622559cd15b79
Felipe <felipecabezas98@gmail.com>
2021-03-26 15:20:45 -0500

    visitor implemented!

---
+++ Behavioral/Visitor/Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public static void main(String[] args) {
+ 
+       ComputerPart computer = new Computer();
+       computer.accept(new ComputerPartVisitorDisplay());
+    }
+ }
